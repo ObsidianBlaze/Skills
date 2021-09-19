ef2ed500d913b391da8950e9f432a2d582bf4754
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2021-09-19 15:50:42 +0100

    Creating the model for all the needed tables.

--- SkillModel.py
+++ SkillModel.py
@@ -8,12 +8,37 @@ from settings import app
 # Creating the object of the database class
 db = SQLAlchemy(app)
 
+# Language model
 class Book(db.Model):
     # Defining the table name
-    __tablename__ = 'skills'
+    __tablename__ = 'language'
         # Defining columns
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), nullable = False)
-    price = db.Column(db.Float, nullable = False)
-    isbn = db.Column(db.Integer)
 
+#Profession_Industries model
+class Book(db.Model):
+    # Defining the table name
+    __tablename__ = 'profession_Industries'
+        # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
+    language_id = db.Column(db.Integer, nullable = False)
+    profession = db.Column(db.String(150), nullable = False)
+
+#Major_organization model
+class Book(db.Model):
+    # Defining the table name
+    __tablename__ = 'major_organizations'
+        # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
+    language_id = db.Column(db.Integer, nullable = False)
+    organization_name = db.Column(db.String(150), nullable = False)
+
+#Specializations
+class Book(db.Model):
+    # Defining the table name
+    __tablename__ = 'specialization'
+        # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
+    language_id = db.Column(db.Integer, nullable = False)
+    specialization = db.Column(db.String(150), nullable = False)
--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, json, jsonify, request, Response
 from settings import *
 
-@app.route('/')
-def hello_world():
-    return "Hello World!"
+skills = [{'python':
+    {
+        'name': 'The Cat In The Hat',
+        'price': 6.99,
+        'isbn': 123456789,
 
+    }
+
+}
+    # Adding dictionaries to hold the some book details
+]
+
+
+# Route to get all skills
+@app.route('/')
+def getAllBooks():
+    return jsonify({'skills': skills})
 
 
 # Used to show error messages
 if __name__ == "__main__":
-      app.run(debug=True)
+    app.run(debug=True)
 
 # Starting the server for the application on port 5000
 app.run(port=5000)
